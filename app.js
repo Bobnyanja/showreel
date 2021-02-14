f274e61934cf7270a6d20609cf0ee37f6fd3b26f
CallMeProteus <jafethyahuma@gmail.com>
2021-02-14 11:08:16 +0300

    modal popup fix

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 
 
-function myfoo(){
-  alert('my foo')
-}
+
 // Get the modal
 var modal = document.getElementById("myModal");
 var content = document.getElementsByClassName("actual-cont")[0];
@@ -38,13 +36,15 @@ fresha.onclick = function() {
 
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+function myfoo(){
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function windowC() {
+  if ( modal.style.display != 'none'){
+    modal.style.display = "none";}
+    else{
+      void(0)
+    }
+  
 }
