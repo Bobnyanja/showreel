7190ae9a9760986f61fd7953a625fe393ed08841
CallMeProteus <jafethyahuma@gmail.com>
2021-02-27 10:32:39 +0300

    player adjusting

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var accelerators = document.getElementById("accelerators");
 var educate = document.getElementById("educate");
 var sanergy = document.getElementById("sanergy");
 var fresha = document.getElementById("fresha");
-var video = document.getElementById("myVideo");
+var video = document.getElementById("playerly");
 var drop = document.getElementById("drop");
 var pauser = document.getElementById("pauseMontage");
 var loader = document.getElementById("loader");
@@ -89,14 +89,13 @@ function playMontage(){
   video.style.display='inherit';
   drop.style.display="none";
   pauser.style.display='inherit';
-  
-  video.play(); };
+   };
 
   function pauseMontage(){
     video.style.display='none';
     drop.style.display="inherit";
     pauser.style.display='none';
-    video.pause(); 
+    
     header.style.opacity=1;
   };
 
@@ -142,7 +141,7 @@ function playMontage(){
             && currentPlayPos > (lastPlayPos + offset)
             && !video.paused
             ) {
-              pauser.innerHTML='pause'
+              pauser.innerHTML='close'
            
             bufferingDetected = false;
             
@@ -150,3 +149,78 @@ function playMontage(){
         lastPlayPos = currentPlayPos
     }
  //movinf profile description   
+
+
+
+
+ //player section/
+ const vidWrapper = document.querySelector('div.player');
+    const myVid = vidWrapper.querySelector('video.player__video');
+    
+    // controls
+    const controlPlay = vidWrapper.querySelector('.player__button');
+    const controlVol = vidWrapper.querySelector('.player__slider[name="volume"]');
+    const controlRate = vidWrapper.querySelector('.player__slider[name="playbackRate"]');
+    const controlSkip = vidWrapper.querySelectorAll('.player__button[data-skip]');
+    const controlFullScreen = vidWrapper.querySelector('.player__fullscreen');
+    const controlProgress = vidWrapper.querySelector('.progress');
+    const progressBar = vidWrapper.querySelector('.progress__filled');
+    
+    // events
+    var drag;
+    var grap;
+    
+    myVid.addEventListener('click', toggleVideo);
+    controlPlay.addEventListener('click', toggleVideo);
+    controlVol.addEventListener('change', updateVol);
+    controlRate.addEventListener('change', updateRate);
+    controlFullScreen.addEventListener('click', goFullScreen);
+    controlSkip.forEach(control => control.addEventListener('click', forward));
+    controlProgress.addEventListener('mouseover', function(){drag = true});
+    controlProgress.addEventListener('mouseout', function(){drag = false; grap = false});
+    controlProgress.addEventListener('mousedown', function(){grap = drag});
+    controlProgress.addEventListener('mouseup', function(){grap = false});
+    controlProgress.addEventListener('click', updateCurrentPos);
+    controlProgress.addEventListener('mousemove', function(e){ if(drag && grap){updateCurrentPos(e)}});
+    
+    var progression;
+    
+    // functions
+    function toggleVideo() {
+      if (myVid.paused) {
+        myVid.play();
+        controlPlay.innerHTML = "❚ ❚";
+        updateProgress();
+        progression = window.setInterval(updateProgress, 200);
+      } else {
+        myVid.pause();
+        controlPlay.innerHTML = "►";
+        clearInterval(progression);
+      };
+    }
+    function updateVol(){
+      var volume = this.value;
+      myVid.volume = volume;
+    }
+    function updateRate(){
+      var rate = this.value;
+      myVid.playbackRate = rate;
+    }
+    function goFullScreen(){
+      console.dir(myVid);
+      if(myVid.webkitSupportsFullscreen) myVid.webkitEnterFullScreen();
+    }
+    function forward(){
+      var value = Number(this.dataset.skip);
+      myVid.currentTime = myVid.currentTime + value;
+    }
+    function updateProgress() {
+      var progress = myVid.currentTime / myVid.duration;
+      progressBar.style.flexBasis = Math.floor(progress * 1000) / 10 + '%';
+    }
+    function updateCurrentPos(e){
+      // offset of the progress bar / video wrapper width
+      var newProgress = (e.clientX - vidWrapper.offsetLeft) / vidWrapper.clientWidth;
+      progressBar.style.flexBasis = Math.floor(newProgress * 1000) / 10 + '%';
+      myVid.currentTime = newProgress * myVid.duration;
+    }
