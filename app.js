ee9c1a3ea638bff4e8d8e5424fbb68b09e6a4a07
CallMeProteus <jafethyahuma@gmail.com>
2021-04-05 17:06:38 +0300

    update 2.2

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-
-
-
 // Get the modal
 var modal = document.getElementById("myModal");
 var content = document.getElementsByClassName("actual-cont")[0];
@@ -85,23 +82,6 @@ function expander() {
    
   }
 }
-function playMontage(){
-  video.style.display='inherit';
-  drop.style.display="none";
-  pauser.style.display='inherit';
-   };
-
-  function pauseMontage(){
-    video.style.display='none';
-    drop.style.display="inherit";
-    pauser.style.display='none';
-    myVid.pause()
-    
-    header.style.opacity=1;
-  };
-
-
-
 
 
     var checkInterval  = 200.0 // check every 50 ms (do not use lower values)
@@ -110,118 +90,45 @@ function playMontage(){
     var bufferingDetected = false
   
     
-    setInterval(checkBuffering, checkInterval)
-    function checkBuffering() {
-        currentPlayPos = video.currentTime
+    // setInterval(checkBuffering, checkInterval)
+    // function checkBuffering() {
+    //     currentPlayPos = video.currentTime
     
-        // checking offset should be at most the check interval
-        // but allow for some margin
-        var offset = (checkInterval - 20) / 1000
+     
+    //     var offset = (checkInterval - 20) / 1000
     
-        // if no buffering is currently detected,
-        // and the position does not seem to increase
-        // and the player isn't manually paused...
-        if (
-                !bufferingDetected 
-                && currentPlayPos < (lastPlayPos + offset)
-                && !video.paused
-            ) {
-              pauser.innerText="Loading video...";
+        
+    //     if (
+    //             !bufferingDetected 
+    //             && currentPlayPos < (lastPlayPos + offset)
+    //             && !video.paused
+    //         ) {
+    //           pauser.innerText="Loading video...";
               
-       
-
-            bufferingDetected = true;
+    //         bufferingDetected = true;
             
 
-        }
+    //     }
     
-        // if we were buffering but the player has advanced,
-        // then there is no buffering
-        if (
-            bufferingDetected 
-            && currentPlayPos > (lastPlayPos + offset)
-            && !video.paused
-            ) {
-              pauser.innerHTML='close'
+    //     if (
+    //         bufferingDetected 
+    //         && currentPlayPos > (lastPlayPos + offset)
+    //         && !video.paused
+    //         ) {
+    //           pauser.innerHTML='close'
            
-            bufferingDetected = false;
+    //         bufferingDetected = false;
             
-        }
-        lastPlayPos = currentPlayPos
-    }
- //movinf profile description   
+    //     }
+    //     lastPlayPos = currentPlayPos
+    // }
+  
 
 
 
 
- //player section/
- const vidWrapper = document.querySelector('div.player');
-    const myVid = vidWrapper.querySelector('video.player__video');
     
-    // controls
-    const controlPlay = vidWrapper.querySelector('.player__button');
-    const controlVol = vidWrapper.querySelector('.player__slider[name="volume"]');
-    const controlRate = vidWrapper.querySelector('.player__slider[name="playbackRate"]');
-    const controlSkip = vidWrapper.querySelectorAll('.player__button[data-skip]');
-    const controlFullScreen = vidWrapper.querySelector('.player__fullscreen');
-    const controlProgress = vidWrapper.querySelector('.progress');
-    const progressBar = vidWrapper.querySelector('.progress__filled');
-    
-    // events
-    var drag;
-    var grap;
-    
-    myVid.addEventListener('click', toggleVideo);
-    controlPlay.addEventListener('click', toggleVideo);
-    controlVol.addEventListener('change', updateVol);
-    controlRate.addEventListener('change', updateRate);
-    controlFullScreen.addEventListener('click', goFullScreen);
-    controlSkip.forEach(control => control.addEventListener('click', forward));
-    controlProgress.addEventListener('mouseover', function(){drag = true});
-    controlProgress.addEventListener('mouseout', function(){drag = false; grap = false});
-    controlProgress.addEventListener('mousedown', function(){grap = drag});
-    controlProgress.addEventListener('mouseup', function(){grap = false});
-    controlProgress.addEventListener('click', updateCurrentPos);
-    controlProgress.addEventListener('mousemove', function(e){ if(drag && grap){updateCurrentPos(e)}});
-    
-    var progression;
-    
+  
     // functions
-    function toggleVideo() {
-      if (myVid.paused) {
-        myVid.play();
-        controlPlay.innerHTML = "❚ ❚";
-        updateProgress();
-        progression = window.setInterval(updateProgress, 200);
-      } else {
-        myVid.pause();
-        controlPlay.innerHTML = "►";
-        clearInterval(progression);
-      };
-    }
-    function updateVol(){
-      var volume = this.value;
-      myVid.volume = volume;
-    }
-    function updateRate(){
-      var rate = this.value;
-      myVid.playbackRate = rate;
-    }
-    function goFullScreen(){
-      console.dir(myVid);
-      if(myVid.webkitSupportsFullscreen) myVid.webkitEnterFullScreen();
-    }
-    function forward(){
-      var value = Number(this.dataset.skip);
-      myVid.currentTime = myVid.currentTime + value;
-    }
-    function updateProgress() {
-      var progress = myVid.currentTime / myVid.duration;
-      progressBar.style.flexBasis = Math.floor(progress * 1000) / 10 + '%';
-    }
-    function updateCurrentPos(e){
-      // offset of the progress bar / video wrapper width
-      var newProgress = (e.clientX - vidWrapper.offsetLeft) / vidWrapper.clientWidth;
-      progressBar.style.flexBasis = Math.floor(newProgress * 1000) / 10 + '%';
-      myVid.currentTime = newProgress * myVid.duration;
-    }
+
+    
